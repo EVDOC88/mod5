2eaea431e06496c8baac99e9c422e8ae2f81cdb9
EVDOC88 <vovan8888@mail.ru>
2023-01-17 22:19:22 +0300

    Задание 5.5.3

--- mod5/Program.cs
+++ mod5/Program.cs
@@ -14,11 +14,16 @@
   Console.ReadKey();
 }
 
-static void Echo(string phrase, int deep) {
-  Console.WriteLine(phrase);
+static void Echo(string saidworld, int deep) {
+        var modif = saidworld;
+        if (modif.Length > 2)
+        {
+            modif = modif.Remove(0, 2);
+        }
+        Console.WriteLine("..." + modif);
 
-  if (deep > 1) {
-    Echo(phrase, deep - 1);
+        if (deep > 1) {
+    Echo(modif, deep - 1);
   }
 }
     }
